fa6d4a89bc5e560760b38c297edd81073ead25b4
Eric <coolspot1000@gmail.com>
2019-09-04 18:50:20 -0500

    Initial REST API

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const request = require('request');
+const qs = require('qs');
+const server = express();
+const webhookURL = process.env.URL
+
+let currentLibrarian;
+
+server.get('/', (req, res) => {
+  res.json({message: "Pong"});
+});
+
+server.get('/checkin', (req, res) => {
+  let json = qs.parse(req);
+  sendCheckIn(json.user_name);
+});
+
+
+function sendCheckIn(username) {
+  let options = {
+    uri: webhookURL,
+    method: 'POST',
+    json: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `${username} is now the librarian`
+        }
+      }
+    ]
+  }
+  request(options, (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+      console.log(body);
+    }
+  })
+}

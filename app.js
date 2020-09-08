e9dc29772c136293213b9fd693468a8531e882bc
Eric <coolspot1000@gmail.com>
2020-09-08 00:36:05 +0000

    Some changes I made a long time ago I dont remember

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const qs = require('qs');
+const db = require('diskdb');
 const server = express();
 const webhookURL = process.env.URL;
 const PORT = process.env.PORT;
+
+db.connect('./data', ['librarian']);
+
 server.use(bodyParser.urlencoded({
   extended:false
 }));
@@ -20,33 +24,66 @@ server.get('/', (req, res) => {
 
 server.post('/checkin', (req, res) => {
   let json = qs.parse(req);
-  console.log(json.body);
-  currentLibrarian = json.body.user_name;
-  sendCheckIn(json.body.response_url, json.body.user_name);
-  console.log(currentLibrarian);
+  let librarians = db.librarian.find();
+  if (librarians.length) {
+      res.status(200);
+      res.type('json');
+    return res.send(`${db.librarian.findOne().name} is already the librarian`);
+  } else {
+  const currLibrarian = {name: `${json.body.user_name}`, current: true};
+    db.librarian.save(currLibrarian);
+  console.log(db.librarian.find());
   res.status(200);
   res.type('json');
   res.send({
   	"response_type": "in_channel",
   	"text": `${json.body.user_name} is now the librarian`
   });
+  }
 });
 
 
 server.post('/checkout', (req, res) => {
   let json = qs.parse(req);
-  console.log(json.body);
-  if (json.body.user_name !== currentLibrarian) {
-	  return res.status(200).send();
-  }
- currentLibrarian = null;
+  let currentLibrarian = db.librarian.findOne();
+    if (currentLibrarian) {
+        if (currentLibrarian.name === json.body.user_name) {
+    db.librarian.remove({current: true});
   res.status(200);
   res.type('json');
   res.send({
 	  "response_type": "in_channel",
 	  "text": `${json.body.user_name} is no longer the librarian`
-  }
-  );
+  });
+} else {
+    res.status(200);
+    res.type('json');
+    res.send('You can not check out of librarian, someone is already checked in');
+}
+} else {
+    res.status(200);
+    res.type('json');
+    res.send('No one is currently the librarian, you can check in to the position');
+}
+});
+
+server.post('/whois', (req, res) => {
+    console.log(db.librarian.find());
+    if (db.librarian.count() === 1) {
+        res.status(200);
+        res.type('json');
+        res.send({
+            "response_type": "in_channel",
+            "text": `${db.librarian.findOne().name} is currently the librarian`
+        });
+    } else {
+        res.status(200);
+        res.type('json');
+        res.send({
+            "response_type": "in_channel",
+            "text": 'No one is currently the librarian'
+        });
+    }
 });
 
 function sendCheckIn(url,username) {
